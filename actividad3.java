5df950797cbbc06b32ff786de093270d9460ebd7
Mex9999 <106620537+Mex9999@users.noreply.github.com>
2022-06-06 11:55:08 -0400

    .
    .

---
+++ actividad3.java
@@ -0,0 +1,62 @@
+
+public static void main(String[] args) {
+        Card card = new Card();
+        int PALOS = card.palo.length;
+        int VALORES = card.valor.length;
+        int COLORES = card.color.length;
+        int N = PALOS * VALORES * COLORES; 
+
+        ArrayList<String> deck = new ArrayList<String>();
+        for (int i = 0; i < VALORES; i++) {
+            for (int j = 0; j < PALOS; j++) {
+                deck.add(card.palo[j]+ ", Rojo , " + card.valor[i]);
+                deck.add(card.palo[j]+ ", Negro , " + card.valor[i]);
+            }
+        }
+
+        Iterator<String> nombreIterator = deck.iterator();
+        while(nombreIterator.hasNext()){
+            String elemento = nombreIterator.next();
+            System.out.print(elemento+" / ");
+        }
+        suflle(deck);
+        head(deck);
+        pick(deck);
+        hand(deck);
+    }
+
+    public static void suflle(ArrayList deck){
+        ArrayList<String> values = deck;
+        Collections.shuffle(values);
+        System.out.println("\n Se mezcló el Deck.");
+        Iterator<String> nombreIterator2 = deck.iterator();
+        while(nombreIterator2.hasNext()){
+            String elemento = nombreIterator2.next();
+            System.out.print(elemento+" / ");
+        }
+    }
+
+    public static void head(ArrayList deck){
+        System.out.println(deck.get(0));
+        deck.remove(0);
+        System.out.println("Quedan " + deck.size() + " cartas \n");
+    }
+
+    public static void pick(ArrayList  deck){
+        Random aleatorio = new Random();
+        System.out.println(deck.get(aleatorio.nextInt(deck.size())));
+        deck.remove(aleatorio.nextInt(deck.size()));
+        System.out.println("Quedan " + deck.size() + " cartas \n");
+    }
+
+    public static void hand(ArrayList deck){
+        for(int i=0; i<=4; i++){
+            System.out.println(deck.get(i));
+        }
+        for(int i=0; i<=4; i++){
+            deck.remove(i);
+        }
+        System.out.println("Quedan " + deck.size()+ " cartas");
+    }
+
+}
